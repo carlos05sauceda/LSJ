1d10137bff3505f83a13cbd342e5903972b37111
pgrod <149545353+pgrod@users.noreply.github.com>
2026-09-10 18:23:38 -0600

    Agregar galeria y oferta academica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,20 @@ export const routes: Routes = [
     },
 
     {
+    path: 'OfertaAcademica',
+    loadComponent: () =>
+      import('./pages/oferta-academica/oferta-academica')
+        .then((m) => m.OfertaAcademica)
+  },
+
+  {
+    path: 'Galeria',
+    loadComponent: () =>
+      import('./pages/galeria/galeria')
+        .then((m) => m.Galeria)
+  },
+
+    {
         path: '',
         redirectTo: 'Home',
         pathMatch: 'full'
