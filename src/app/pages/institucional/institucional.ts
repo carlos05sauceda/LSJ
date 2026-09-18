3f0c3d944f9da1de111448eb42700b8c1bda6e28
carlos05sauceda <carlos05sauceda@gmail.com>
2026-09-18 09:19:27 -0600

    .

--- src/app/pages/institucional/institucional.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-institucional',
-  styleUrl: './institucional.css',
-  templateUrl: './institucional.html',
-})
-export class Institucional {}
